b04b75c5447a23d86c04890c6691a96d7e52e1e1
ahmard <ahmard06@gmail.com>
2022-02-13 20:29:16 +0100

    Initial Commit

---
+++ src/InstanceCreator.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RTC\Utils;
+
+use JetBrains\PhpStorm\Pure;
+
+trait InstanceCreator
+{
+    /**
+     * @var static $_instance
+     */
+    protected static $instance;
+
+
+    #[Pure] public static function create(...$arguments): static
+    {
+        return new static(...$arguments);
+    }
+
+    public static function createSingleton(...$arguments): static
+    {
+        if (!isset(static::$instance)) {
+            return static::$instance = self::create(...$arguments);
+        }
+
+        return self::$instance;
+    }
+}
